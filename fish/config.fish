aa82f4461558f7fdb96250aa5dde80b9365f6fb4
YaQia <yyaqia@gmail.com>
2023-08-23 10:59:00 +0800

    updated environment variables in fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
-set PATH ~/.local/bin $PATH
-set NEMU_HOME /home/ethan/ics2022/nemu
-set AM_HOME /home/ethan/ics2022/abstract-machine
-set NAVY_HOME /home/ethan/ics2022/navy-apps
+set -x PATH ~/.local/bin $PATH
+set -x NEMU_HOME /home/ethan/ics2022/nemu
+set -x AM_HOME /home/ethan/ics2022/abstract-machine
+set -x NAVY_HOME /home/ethan/ics2022/navy-apps
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
