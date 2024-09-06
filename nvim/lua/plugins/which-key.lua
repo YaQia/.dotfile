9ff0be011834a590b0cc3be4b661f693f32b7508
Ya Qia <yyaqia@gmail.com>
2024-09-06 19:16:00 +0800

    feat: add outline.lua to replace lspsaga

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -50,7 +50,7 @@ local config = function()
 		{ "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", desc = "Rename" },
 		{ "<leader>ls", "<cmd>Telescope lsp_document_symbols<cr>", desc = "Document Symbols" },
 		{ "<leader>lS", "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", desc = "Workspace Symbols" },
-		{ "<leader>lo", "<cmd>Lspsaga outline<cr>", desc = "Outline" },
+		{ "<leader>lo", "<cmd>Outline<cr>", desc = "Outline" },
 		-- Search group
 		{ "<leader>s", group = "Search" },
 		{ "<leader>sf", "<cmd>Telescope find_files<cr>", desc = "Files" },
