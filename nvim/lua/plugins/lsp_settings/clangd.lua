b9517ca1cfc687104eedfedea90dc453294f7cdc
Ya Qia <yyaqia@gmail.com>
2024-09-07 11:38:39 +0800

    feat: update lsp config

--- nvim/lua/plugins/lsp_settings/clangd.lua
+++
@@ -1,7 +0,0 @@
-return {
-  capabilities = {
-    -- because formatter clang-format needs one particular offsetEncoding, the default offsetEncoding is a list 
-    -- (source code: https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/clangd.lua)
-    offsetEncoding = 'utf-8',
-  },
-}
