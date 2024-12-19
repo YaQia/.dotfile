61fdb23e791311e7369cd42c0e98cafc85b4ed61
Ya Qia <yyaqia@gmail.com>
2024-12-19 17:01:11 +0800

    chore: remove redundant config in lualine.lua

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -89,114 +89,9 @@ local spaces = function()
 	return "󰌒 " .. vim.api.nvim_get_option_value("tabstop", {})
 end
 
--- theme
--- stylua: ignore
--- local colors
--- if vim.o.background == "dark" then
--- local colors = {
--- 	blue = "#569cd6",
--- 	green = "#6a9955",
--- 	purple = "#c586c0",
--- 	red = "#d16969",
--- 	yellow = "#dcdcaa",
--- 	orange = "#D7BA7D",
--- 	fg = "#ababab",
--- 	-- bg     = '#007acc',
--- 	-- bg     = '#68217a',
--- 	bg = "#252525",
--- 	gray = "#333333",
--- 	-- light_gray  = '#5c6370',
--- 	gray3 = "#3e4452",
--- }
--- else
--- 	colors = {
--- 		blue = "#569cd6",
--- 		green = "#6a9955",
--- 		purple = "#c586c0",
--- 		red = "#d16969",
--- 		yellow = "#dcdcaa",
--- 		orange = "#D7BA7D",
--- 		fg = "#ababab",
--- 		-- bg     = '#007acc',
--- 		-- bg     = '#68217a',
--- 		bg = "#e4e4e4",
--- 		gray = "#f5f5f5",
--- 		-- light_gray  = '#5c6370',
--- 		gray3 = "#3e4452",
--- 	}
--- end
-local colors = {
-	blue        = '#569cd6',
-	green       = '#6a9955',
-	purple      = '#c586c0',
-	red         = '#d16969',
-	yellow      = '#dcdcaa',
-	orange      = '#D7BA7D',
-	fg          = '#ababab',
-	-- bg     = '#007acc',
-	-- bg     = '#68217a',
-	bg          = '#252525',
-	gray        = '#333333',
-	light_gray  = '#5c6370',
-	gray3       = '#3e4452',
-	inactive_fg = '#6f6f6f',
-}
--- local bubbles_theme = {
--- 	normal = {
--- 		a = { fg = colors.bg, bg = colors.blue },
--- 		b = { fg = colors.blue, bg = colors.gray },
--- 		c = { fg = colors.fg, bg = colors.bg },
--- 	},
--- 	insert = { a = { fg = colors.bg, bg = colors.green }, b = { fg = colors.green, bg = colors.gray } },
--- 	visual = { a = { fg = colors.bg, bg = colors.purple }, b = { fg = colors.purple, bg = colors.gray } },
--- 	command = { a = { fg = colors.bg, bg = colors.orange }, b = { fg = colors.orange, bg = colors.gray } },
--- 	replace = { a = { fg = colors.bg, bg = colors.red }, b = { fg = colors.red, bg = colors.gray } },
---
--- 	inactive = {
--- 		a = { bg = colors.bg, fg = colors.blue },
--- 		b = { bg = colors.bg, fg = colors.gray, gui = "bold" },
--- 		c = { bg = colors.bg, fg = colors.gray },
--- 	},
--- }
-local bubble_theme = {
-	normal = {
-		a = { fg = colors.bg, bg = colors.blue },
-		b = { fg = colors.blue, bg = colors.gray },
-		c = { fg = colors.fg, bg = colors.bg },
-		y = { fg = colors.blue, bg = colors.gray },
-	},
-	insert = {
-		a = { fg = colors.bg, bg = colors.green },
-		b = { fg = colors.green, bg = colors.gray },
-		y = { fg = colors.green, bg = colors.gray },
-	},
-	visual = {
-		a = { fg = colors.bg, bg = colors.purple },
-		b = { fg = colors.purple, bg = colors.gray },
-		y = { fg = colors.purple, bg = colors.gray },
-	},
-	command = {
-		a = { fg = colors.bg, bg = colors.orange },
-		b = { fg = colors.orange, bg = colors.gray },
-		y = { fg = colors.orange, bg = colors.gray },
-	},
-	replace = {
-		a = { fg = colors.bg, bg = colors.red },
-		b = { fg = colors.red, bg = colors.gray },
-		y = { fg = colors.red, bg = colors.gray },
-	},
-
-	inactive = {
-		a = { bg = colors.bg, fg = colors.blue },
-		-- gui = "bold"
-		b = { bg = colors.gray, fg = colors.inactive_fg },
-		c = { bg = colors.bg, fg = colors.gray },
-	},
-}
-
 local opts = {
 	options = {
-		theme = bubble_theme,
+		theme = "auto",
 		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
 		-- section_separators = { left = "", right = "" },
@@ -242,7 +137,7 @@ local opts = {
 				-- tabs_color = {
 				-- 	inactive = 'lualine_c_active',
 				-- },
-				separator = { left = "", right = "" },
+				separator = { right = "" },
 				-- padding = { left = 2, right = 2 },
 			},
 		},
