b6f99d24724924f79d13667515d49992b8873909
Ya Qia <yyaqia@gmail.com>
2025-08-19 17:03:53 +0800

    feat(proxy): only set all_proxy so that Firefox will not crash
    Firefox will refuse to connect after you close the proxy app if you
    configured http_proxy or https_proxy. But I choose to use all_proxy
    only, by doing so, Firefox works well even if the proxy app is closed.
    However terminal might still be broken after closing the proxy app.

--- fish/config.fish
+++ fish/config.fish
@@ -3,9 +3,9 @@ set -x DEBUGINFOD_URLS "https://debuginfod.archlinux.org"
 # set -x DEEPSEEK_API_KEY '0eP9ifBPr7HkQaQElVcAHphcFALacxiNUYWDqAdbU8kngyQ_B4ueOL2_K-EgtI4m5KfnJH_HWySzGwqrMiaH1w'
 # set -x OPENROUTER_API_KEY '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'
 source ~/.config/fish/private_apis.fish
-set -x http_proxy http://127.0.0.1:7892
-set -x https_proxy http://127.0.0.1:7892
-set -x all_proxy socks5://127.0.0.1:7891
+# set -x http_proxy http://127.0.0.1:7892
+# set -x https_proxy http://127.0.0.1:7892
+set -x all_proxy socks5h://127.0.0.1:7891
 set -x EDITOR /bin/nvim
 set -gx HSA_OVERRIDE_GFX_VERSION 11.0.0
 set -gx ROCM_PATH /opt/rocm
